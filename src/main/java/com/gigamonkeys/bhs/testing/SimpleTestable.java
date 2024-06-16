0d58bde7c452e04129efe491dc08e968960d2e0d
Peter Seibel <peter@gigamonkeys.com>
2024-06-15 21:18:40 -0700

    Add SimpleTestable implementation of Testable

---
+++ src/main/java/com/gigamonkeys/bhs/testing/SimpleTestable.java
@@ -0,0 +1,25 @@
+package com.gigamonkeys.bhs.testing;
+
+import com.gigamonkeys.bhs.BespokeTestRunner;
+import java.util.Map;
+import java.util.function.Supplier;
+
+/** Implementation of Testable that pushes all the work to the tests which just supply results. */
+public class SimpleTestable implements Testable {
+
+  private final String name;
+  private final Map<String, Supplier<BespokeTestRunner.TestResult[]>> tests;
+
+  public SimpleTestable(String name, Map<String, Supplier<BespokeTestRunner.TestResult[]>> tests) {
+    this.name = name;
+    this.tests = tests;
+  }
+
+  public String name() {
+    return this.name;
+  }
+
+  public BespokeTestRunner.TestResult[] results() throws Exception {
+    return tests.get(name).get();
+  }
+}
